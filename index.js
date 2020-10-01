58b72af3f759d0b6762f68788fbe992440cd1a4a
Adeeb Ahmad <adeebahmad0010@gmail.com>
2020-10-02 03:22:11 +0500

    You may like section completed

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // ! PRODUCT BUTTONS
 const product_buttons = document.querySelectorAll(".products_buttons_wrapper button")
 const product_container = document.querySelectorAll(".products_wrapper")
-
+const you_may_like_image_selector = document.querySelectorAll('.image_wrapper');
+const you_may_like_main_img = document.querySelector('.main_img');
+const cart_item_number = document.querySelector('.cart_item_number');
 
 // ? SWIPER JS INITIALIZATION
 var mySwiper = new Swiper('.swiper-container', {
@@ -15,11 +17,28 @@ var mySwiper = new Swiper('.swiper-container', {
 })
 
 
+you_may_like_image_selector.forEach(el=>{
+  el.addEventListener("click",()=>{
+    you_may_like_image_selector.forEach(el=> el.classList.remove("active"));
+    const img = el.querySelector("img");
+    you_may_like_main_img.src = img.src;
+    el.classList.add("active");
+  })
+})
 
 product_buttons.forEach(el=>{
   el.addEventListener("click",()=>{
-    [product_buttons,product_container].forEach(nodeList=> nodeList.forEach(el=> el.classList.remove("active")))
-    document.querySelector(el.dataset.target).classList.add("active")
-    el.classList.add("active")
-  })
+    [product_buttons,product_container].forEach(nodeList=> nodeList.forEach(el=> el.classList.remove("active")));
+    document.querySelector(el.dataset.target).classList.add("active");
+    el.classList.add("active");
+  });
+});
+
+[cart_item_number.querySelector(".plus"),cart_item_number.querySelector(".minus")].forEach(el=>{
+  const num = cart_item_number.querySelector(".value");
+  el.addEventListener('click',()=>{
+    if(+num.textContent >= 1 || el.dataset.sign != "-"){
+      num.textContent = eval(`${num.textContent+el.dataset.sign + "1"}`)
+    }
+  });
 })
